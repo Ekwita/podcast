38a15ce3ab221d2c6633821a94c4c5c374e31a99
Wojciech Gałek <wgalek24@wp.pl>
2024-10-26 17:26:37 +0200

    Create new Models, migrations and Controllers

--- app/Models/Episode.php
+++ app/Models/Episode.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Str;
 
 class Episode extends Model
@@ -21,4 +23,14 @@ class Episode extends Model
         'host_name',
         'guest_names',
     ];
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
